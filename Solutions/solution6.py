221179c613c206d9e93652043f947fd2dcdb857c
jijinkh <jijinkharidas10@gmail.com>
2019-09-09 07:46:22 +0530

    added author name

--- Solutions/solution6.py
+++ Solutions/solution6.py
@@ -1,20 +1,26 @@
-# Author : jijinkh
+# Author : Abilash1907
+def compare(s1, s2):
+    m = len(s1)
+    n = len(s2)
+    j = 0
+    i = 0
+    while (i < n and j < m):
+        if (s1[j] == s2[i]):
+            j += 1
+        i += 1
+    return (j == m)
+
+
+def longest(d, st):
+    r = ""
+    le = 0
+    for i in d:
+        if (le < len(i) and compare(i, st)):
+            r = i
+            le = len(i)
+    return r
 n = int(input())
-x = [z for z in input().split()][:n]
-stri = input()
-
-
-def exists(a, b):
-    pos = 0
-    for ch in a:
-        if pos < len(b) and ch == b[pos]:
-            pos += 1
-    return pos == len(b)
-
-
-res = ""
-for i in x:
-    if exists(stri, i):
-        if(len(res) < len(i)):
-            res = i
-print(res)
+l = input("dict=").split(' ')
+l = {n for n in l}
+s = input("str=")
+print(longest(l, s))
